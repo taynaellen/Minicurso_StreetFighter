121dc3460ce4925e19d8b0f17e3394b20e985a8f
Tayná <taynaellen1@gmail.com>
2024-03-14 14:27:22 -0300

    iniciando página de personagem

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
+// Alterar altura do header quando a tela for scrolada
 window.addEventListener('scroll', function() {
     var header = document.getElementsByTagName("header")[0];
-    if (window.scrollY > 0) {
+    if (window.scrollY > 100) {
         header.style.height = '3rem';
     } else {
         header.style.height = '5rem';
     }
 });
 
-
+// Alterar img dos personagens quando passar o cursor
 var imagemP1 = document.getElementsByClassName("p1")[0];
 imagemP1.onmouseover = function () {
     this.src = "/img/select_character21_over.png";
@@ -15,8 +16,6 @@ imagemP1.onmouseover = function () {
 imagemP1.onmouseout = function () {
     this.src = "/img/select_character21.png";
 };
-
-
 
 var imagemP2 = document.getElementsByClassName("p2")[0];
 imagemP2.onmouseover = function () {
@@ -26,8 +25,6 @@ imagemP2.onmouseout = function () {
     this.src = "/img/select_character20.png";
 };
 
-
-
 var imagemP3 = document.getElementsByClassName("p3")[0];
 imagemP3.onmouseover = function () {
     this.src = "/img/select_character19_over.png";
@@ -35,8 +32,6 @@ imagemP3.onmouseover = function () {
 imagemP3.onmouseout = function () {
     this.src = "/img/select_character19.png";
 };
-
-
 
 var imagemP4 = document.getElementsByClassName("p4")[0];
 imagemP4.onmouseover = function () {
@@ -46,3 +41,16 @@ imagemP4.onmouseout = function () {
     this.src = "/img/select_character18.png";
 };
 
+
+// Transition do titulo
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').classList.add('transicaoh1');
+});
+
+// Transition das imagens dos personagens
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.p1').classList.add('transicaoimg');
+    document.querySelector('.p2').classList.add('transicaoimg');
+    document.querySelector('.p3').classList.add('transicaoimg');
+    document.querySelector('.p4').classList.add('transicaoimg');
+});
